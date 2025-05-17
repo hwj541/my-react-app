de58ac00789215378f60aa5fde88df53d421bf5a
wenjun03.huang <wenjun03.huang@horizon.cc>
2025-05-17 23:04:00 +0800

    feat(viewer): [cr_id_skip] F提测 no_3rd_skip

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.change0515
-        </p>
+        <p>哈哈哈哈哈我是黄文俊</p>
         <a
           className="App-link"
           href="https://reactjs.org"
